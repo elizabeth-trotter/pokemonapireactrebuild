4a0f974082544856d9d3d4335f37a3d0c36b51f6
et120 <e.trotter120@gmail.com>
2024-03-16 14:40:03 -0700

    favorites list to modal add

--- src/Components/Modal/ModalComponent.tsx
+++ src/Components/Modal/ModalComponent.tsx
@@ -2,9 +2,16 @@
 
 import { Button, Modal } from 'flowbite-react';
 import { useState } from 'react';
+import { getLocalStorage } from '../../Utils/localstorage';
 
 function ModalComponent() {
   const [openModal, setOpenModal] = useState(false);
+  const favorites = getLocalStorage();
+
+  // Formatting
+  function capitalizeFirstLetter(str: string) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
 
   return (
     <>
@@ -16,22 +23,12 @@ function ModalComponent() {
         <Modal.Header>Favorite Pokémon</Modal.Header>
         <Modal.Body>
           <div className="space-y-6">
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
-              companies around the world are updating their terms of service agreements to comply.
-            </p>
-            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
-              to ensure a common set of data rights in the European Union. It requires organizations to notify users as
-              soon as possible of high-risk data breaches that could personally affect them.
-            </p>
+            {favorites.map((fav: string) => {
+              return <p>{capitalizeFirstLetter(fav)}</p>;
+            })}
           </div>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={() => setOpenModal(false)}>I accept</Button>
-          <Button color="gray" onClick={() => setOpenModal(false)}>
-            Decline
-          </Button>
         </Modal.Footer>
       </Modal>
     </>
